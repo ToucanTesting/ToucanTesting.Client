4fa7d2c998b742ee031cd988ece87bb3584b6e70
Daniel Breen <daninreallife7@gmail.com>
2018-03-17 15:50:55 -0600

    Style Updates

--- src/app/components/shared/buttons/buttons.component.ts
+++ src/app/components/shared/buttons/buttons.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'tt-buttons',
     templateUrl: './buttons.component.html',
-    styleUrls: ['./buttons.component.css']
+    styleUrls: ['./buttons.component.scss']
 })
 export class ButtonComponent {
 
