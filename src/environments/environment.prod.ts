79f799c03ad2842b95222b92589df12f29d90295
Daniel Breen <daninreallife7@gmail.com>
2018-04-18 08:21:09 -0600

    Update baseUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   envName: 'prod',
-  baseUrl: 'http://toucantesting.com/',
+  baseUrl: 'http://app.toucantesting.com/',
   apiUrl: '//api.toucantesting.com/'
 };
