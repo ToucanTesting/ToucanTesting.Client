b3f10e4ba470c36480d6ed80a5377b98b7e29105
Daniel Breen <daninreallife7@gmail.com>
2018-04-19 09:39:14 -0600

    Update Dependencies

--- src/app/services/api-interceptor.ts
+++ src/app/services/api-interceptor.ts
@@ -6,7 +6,7 @@ import { environment } from 'environments/environment';
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
+    console.log('interceptor log');
     const apiReq = req.clone({ url: `${environment.apiUrl}${req.url}` });
     return next.handle(apiReq);
   }
