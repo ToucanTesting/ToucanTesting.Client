713159877b22dd24b5772058617aa32b5e745a74
Daniel Breen <daninreallife7@gmail.com>
2018-04-19 06:51:03 -0600

    Update Audience

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   envName: 'prod',
   baseUrl: 'http://app.toucantesting.com/',
-  apiUrl: 'https://api.toucantesting.com/'
+  apiUrl: 'http://api.toucantesting.com/'
 };
