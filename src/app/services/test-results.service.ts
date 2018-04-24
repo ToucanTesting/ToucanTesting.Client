d091ab687dbceae40d86264d4bd7d91b03fad70a
Daniel Breen <daninreallife7@gmail.com>
2018-04-24 00:29:46 -0600

    Split TestResults Upsert into Create/Update

--- src/app/services/test-results.service.ts
+++ src/app/services/test-results.service.ts
@@ -9,15 +9,15 @@ export class TestResultsService {
 
     }
 
-    public createTestResult(result: TestResult): Observable<TestResult> {
-        return this.http.post<TestResult>(`test-results`, result);
+    public createTestResult(testResult: TestResult): Observable<TestResult> {
+        return this.http.post<TestResult>(`test-results`, testResult);
     }
 
     public getTestResults(testRunId: number): Observable<TestResult[]> {
         return this.http.get<TestResult[]>(`test-results?testRunId=${testRunId}`);
     }
 
-    public upsertTestResult(result: TestResult): Observable<TestResult> {
-        return this.http.put<TestResult>(`test-results`, result);
+    public updateTestResult(testResult: TestResult): Observable<TestResult> {
+        return this.http.put<TestResult>(`test-results/${testResult.id}`, testResult);
     }
 }
