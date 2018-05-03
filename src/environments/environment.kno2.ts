053319eb7394020e6c2271b0b5483859dfa491f7
Daniel Breen <daniel.breen@royaljay.net>
2018-05-03 11:13:59 -0600

    Add Kno2 Environment

--- src/environments/environment.kno2.ts
+++ src/environments/environment.kno2.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   envName: 'kno2',
   baseUrl: 'https://toucan-testing-test.azurewebsites.net/',
-  apiUrl: 'http://api.toucantesting.com/'
+  apiUrl: 'https://as-kno2toucanapi-dev.azurewebsites.net/'
 };
