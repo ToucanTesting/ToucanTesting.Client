930b826ee5affda6b343e0e58c8df5286da0959f
Daniel Breen <daniel.breen@royaljay.net>
2018-05-03 15:00:13 -0600

    Fix kno2 baseUrl

--- src/environments/environment.kno2.ts
+++ src/environments/environment.kno2.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   envName: 'kno2',
-  baseUrl: 'https://toucan-testing-test.azurewebsites.net/',
-  apiUrl: 'https://as-kno2toucanapi-dev.azurewebsites.net/'
+  baseUrl: 'https://as-kno2toucanweb-dev.azurewebsites.net/',
+  apiUrl: 'https://as-kno2toucanapi-dev.azurewebsites.net/',
+  whitelist: /^null$'/
 };
