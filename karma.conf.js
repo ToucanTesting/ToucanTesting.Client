d29d0a60709c97991631a0f852d24f2c69eea81f
Daniel Breen <dapperdandev@gmail.com>
2018-05-25 00:20:10 -0600

    More Karma Adjustments

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,8 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-teamcity-reporter')
+      require('karma-teamcity-reporter'),
+      require('karma-phantomjs-launcher')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -29,6 +30,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['PhantomJS'],
-    singleRun: false
+    singleRun: true
   });
 };
